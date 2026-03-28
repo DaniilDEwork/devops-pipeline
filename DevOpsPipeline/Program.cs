8225c34ff2ae23f95b391f1b0863a24d0181014c
DaniilDEwork <denisenkode24@st.ithub.ru>
2026-03-29 00:37:47 +0300

    Create base C# project structure

---
+++ DevOpsPipeline/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Учебный проект по DevOps");
+        Console.WriteLine("Денисенко Даниила Евгеньевича");
+        Console.WriteLine("3п4.23 NET");
+    }
+}
