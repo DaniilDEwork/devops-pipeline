abe33bc0a8e34c0aa92bb774ac034602e67f7915
DaniilDEwork <denisenkode24@st.ithub.ru>
2026-03-29 02:31:03 +0300

    Update Program.cs

--- DevOpsPipeline/Program.cs
+++ DevOpsPipeline/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Учебный проект для комиссии по дисциплине \"Введение в DevOps\"");
+        Console.WriteLine("Учебный проект для комиссии по дисциплине \"Введение В DevOps\"");
         Console.WriteLine("Ученик: Денисенко Даниил Евгеньевич");
         Console.WriteLine("Группа: \"3п4.23 NET\"");
     }
